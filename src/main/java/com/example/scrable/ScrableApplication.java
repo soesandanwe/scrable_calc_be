feb07ed0ddd17933dd460f92541c47f2a2df31b5
Soe Sanda Nwe <soesandanwe@gmail.com>
2023-07-17 19:52:24 +0800

    init

---
+++ src/main/java/com/example/scrable/ScrableApplication.java
@@ -0,0 +1,13 @@
+package com.example.scrable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrableApplication.class, args);
+	}
+
+}
